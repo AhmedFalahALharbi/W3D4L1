23aebc98e443ecd608fc2940b413fc7a168584c0
AhmedFalahALharbi <ahmedfalahalharbi@gmail.com>
2024-09-11 11:53:58 +0300

    create file

---
+++ main.js
@@ -0,0 +1,20 @@
+
+title = document.getElementsByClassName("page-title")[0]
+title.style.color = "lightskyblue"
+title.style.backgroundColor = "lightgrey"
+// Q2
+name1 = document.getElementById("name")
+name1.style.fontWeight = 900;
+// Q3
+console.log(title);
+image = document.getElementsByTagName('img')[0].setAttribute('src' , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ7fTFb3G8JZJy_oWg4rfoketLxdUnUF2eLw&s")
+
+// Q4
+list = document.querySelector("ul") 
+list.innerHTML =
+    '
+<li></li>
+        <li></li>
+        <li></li>';
+
+
